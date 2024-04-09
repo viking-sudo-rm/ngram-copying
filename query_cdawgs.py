3ab03fc2cb75376b6ca9391044376b9281b4893e
Will Merrill <vikingarnir.will@gmail.com>
2024-04-09 15:04:38 -0700

    Debug script to query CDAWGs

--- query_cdawgs.py
+++ query_cdawgs.py
@@ -8,29 +8,34 @@ python query_cdawgs.py \
 
 import argparse
 import json
+import asyncio
 from src.async_client import AsyncRustyDawgClient
-
-MACHINES = [f"localhost:{port}" for port in range(5000, 5030)]
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("text_path", type=str)
     parser.add_argument("save_path", type=str)
+    parser.add_argument("--start_port", type=int, default=5000)
+    parser.add_argument("--end_port", type=int, default=5030)
     return parser.parse_args()
 
-def main(args):
+async def main(args):
     # Assumes the input is in .jsonl format with a "text" field.
     with open(args.text_path, "r") as fh:
         blobs = [json.loads(line) for line in fh]
         texts = [blob["text"] for blob in blobs]
-    
+    print("Texts:", texts)
+
     # Create a client to pass text through the API.
-    client = AsyncRustyDawgClient(MACHINES)
-    results = client.query(texts)
+    hosts = [f"localhost:{port}" for port in range(args.start_port, args.end_port)]
+    print("Hosts:", hosts)
+    client = AsyncRustyDawgClient(hosts)
+    results = await client.query(texts)
 
     # Save results in .json format to the specified file. Document order will be the same as in input file.
     with open(args.save_path, "w") as fh:
-        print(json.dumps(results, fh))
+        json.dump(results, fh)
 
 if __name__ == "__main__":
-    main(parse_args())
+    args = parse_args()
+    asyncio.run(main(args))
