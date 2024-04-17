ee918844cc39f482c27ed198115a86c5cad88811
Will Merrill <willm@allenai.org>
2024-04-17 15:06:33 -0700

    Update paths for prompts

--- scripts/generate_by_model.sh
+++ scripts/generate_by_model.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 ROOT=${ROOT:-"/net/nfs.cirrascale/allennlp/willm/ngram-copying"}
-PROMPTS_PATH=$ROOT/prompts.jsonl
+PROMPTS_PATH=$ROOT/data/by-domain/prompts.jsonl
 OUT_DIR=$ROOT/lm-generations
 N_TOKENS=1000
 
 SUFFIX=""
 # SUFFIX="-deduped"
+DIR=$OUT_DIR/models$SUFFIX
 
 # Fail if already exists.
 mkdir $OUT_DIR
 
 
-echo "========== Generating 'models$suffix' =========="
+echo "========== Generating 'models$SUFFIX' =========="
 SIZES=("70m" "160m" "410m" "1b" "1.4b" "2.8b" "6.9b" "12b")
-mkdir $OUT_DIR/models$suffix
+mkdir $DIR
 for idx in "${!SIZES[@]}"; do
     size=${SIZES[idx]}
-    MODEL="pythia-$size$suffix"
-    echo "===== ${MODEL} ====="
+    model="pythia-$size$SUFFIX"
+    echo "===== ${model} ====="
     python generate_from_lm.py \
         EleutherAI/${MODEL} \
         $PROMPTS_PATH \
-        $OUT_DIR/models$suffix/${MODEL}.jsonl \
+        $DIR/${model}.jsonl \
         --n_tokens=$N_TOKENS \
         --sample
 done
-cat $OUT_DIR/models$suffix/*.jsonl > $OUT_DIR/models$suffix.jsonl
+cat $DIR/*.jsonl > $OUT_DIR/models$SUFFIX.jsonl
