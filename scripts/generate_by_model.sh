3f25b688c37f49fb577c72e090bedc0d50f53a91
Will Merrill <willm@allenai.org>
2024-04-18 10:17:27 -0700

    Update scripts

--- scripts/generate_by_model.sh
+++ scripts/generate_by_model.sh
@@ -19,7 +19,7 @@ mkdir $DIR
 for idx in "${!SIZES[@]}"; do
     model="pythia-${SIZES[idx]}$SUFFIX"
     echo "===== $model ====="
-    printf "\n" | gantry run \
+    printf "$model" | gantry run \
         --workspace ai2/rusty-dawg \
         --cluster ai2/allennlp-cirrascale \
         --venv base \
